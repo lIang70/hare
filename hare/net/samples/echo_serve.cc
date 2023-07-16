9835ecc6fe5e8ad08fd5ae9b296cfcc2f005e3b2
l1ang70 <gog_017@outlook.com>
2023-07-16 22:01:46 +0800

    [update] update

--- hare/net/samples/echo_serve.cc
+++ hare/net/samples/echo_serve.cc
@@ -1,6 +1,8 @@
 #include <hare/base/io/console.h>
 #include <hare/base/io/cycle.h>
-#include <hare/base/logging.h>
+#include <hare/base/util/system.h>
+#include <hare/log/backends/file_backend.h>
+#include <hare/log/registry.h>
 #include <hare/net/acceptor.h>
 #include <hare/net/hybrid_serve.h>
 #include <hare/net/tcp_session.h>
@@ -8,7 +10,10 @@
 
 #include <map>
 
+#if defined(H_OS_WIN32)
+#else
 #include <sys/socket.h>
+#endif
 
 #define USAGE "echo_serve -p [port] [udp/tcp]"
 
@@ -17,21 +22,23 @@ using hare::net::session;
 using hare::net::tcp_session;
 using hare::net::udp_session;
 
+static hare::ptr<hare::log::logger> server_logger {};
+
 static void new_session(const session::ptr& _ses, hare::timestamp _ts, const acceptor::ptr& _acc)
 {
     if (_acc->type() == hare::net::TYPE_TCP) {
         _ses->set_connect_callback([=](const session::ptr& _session, uint8_t _event) {
             if ((_event & hare::net::SESSION_CONNECTED) != 0) {
-                LOG_INFO() << "session[" << _session->name() << "] is connected.";
+                LOG_INFO(server_logger, "session[{}] is connected.", _session->name());
             }
             if ((_event & hare::net::SESSION_CLOSED) != 0) {
-                LOG_INFO() << "session[" << _session->name() << "] is disconnected.";
+                LOG_INFO(server_logger, "session[{}] is disconnected.", _session->name());
             }
         });
 
         auto tsession = std::static_pointer_cast<tcp_session>(_ses);
 
-        tsession->set_read_callback([](const hare::ptr<tcp_session>& _session, hare::io::buffer& _buffer, const hare::timestamp&) {
+        tsession->set_read_callback([](const hare::ptr<tcp_session>& _session, hare::net::buffer& _buffer, const hare::timestamp&) {
             _session->append(_buffer);
         });
 
@@ -40,42 +47,54 @@ static void new_session(const session::ptr& _ses, hare::timestamp _ts, const acc
         });
 
         tsession->set_high_water_callback([=](const hare::ptr<tcp_session>& _session) {
-            LOG_ERROR() << "session[" << _session->name() << "] is going offline because it is no longer receiving data.";
+            LOG_ERROR(server_logger, "session[{}] is going offline because it is no longer receiving data.", _session->name());
             _session->force_close();
         });
 
-        LOG_INFO() << "recv a new session[" << _ses->name() << "] at " << _ts.to_fmt(true) << " on acceptor=" << _acc->fd();
+        LOG_INFO(server_logger, "recv a new tcp-session[{}] at {} on acceptor={}.", _ses->name(), _ts.to_fmt(true), _acc->fd());
     } else if (_acc->type() == hare::net::TYPE_UDP) {
         _ses->set_connect_callback([=](const session::ptr& _session, uint8_t _event) {
             if ((_event & hare::net::SESSION_CONNECTED) != 0) {
-                LOG_INFO() << "session[" << _session->name() << "] is connected.";
+                LOG_INFO(server_logger, "session[{}] is connected.", _session->name());
             }
             if ((_event & hare::net::SESSION_CLOSED) != 0) {
-                LOG_INFO() << "session[" << _session->name() << "] is disconnected.";
+                LOG_INFO(server_logger, "session[{}] is disconnected.", _session->name());
             }
         });
 
         auto usession = std::static_pointer_cast<udp_session>(_ses);
 
-        usession->set_read_callback([](const hare::ptr<udp_session>& _session, hare::io::buffer& _buffer, const hare::timestamp&) {
+        usession->set_read_callback([](const hare::ptr<udp_session>& _session, hare::net::buffer& _buffer, const hare::timestamp&) {
             _session->append(_buffer);
         });
 
         usession->set_write_callback([=](const hare::ptr<udp_session>& _session) {
-            
+
         });
 
-        LOG_INFO() << "recv a new session[" << _ses->name() << "] at " << _ts.to_fmt(true) << " on acceptor=" << _acc->fd();
+        LOG_INFO(server_logger, "recv a new udp-session[{}] at {} on acceptor={}.", _ses->name(), _ts.to_fmt(true), _acc->fd());
     }
 }
 
-auto main(int32_t argc, char** argv) -> int32_t
+auto main(std::int32_t argc, char** argv) -> std::int32_t
 {
-    hare::logger::set_level(hare::log::LEVEL_TRACE);
+    using hare::log::backend;
+    using hare::log::file_backend_st;
+    using hare::log::detail::rotate_file;
 
     if (argc < 4) {
-        LOG_FATAL() << USAGE;
+        fmt::println(USAGE);
+        return (0); 
     }
+
+    constexpr std::uint64_t file_size = static_cast<std::uint64_t>(64) * 1024 * 1024;
+
+    auto tmp = hare::util::system_dir();
+    std::vector<hare::ptr<backend>> backends {
+        std::make_shared<file_backend_st<rotate_file<file_size>>>(tmp + "/echo_serve")
+    };
+
+    server_logger = hare::log::registry::create("echo_serve", backends.begin(), backends.end());
 
     hare::net::TYPE acceptor_type = hare::net::TYPE_INVALID;
     if (std::string(argv[3]) == "tcp") {
@@ -92,17 +111,17 @@ auto main(int32_t argc, char** argv) -> int32_t
     main_serve->set_new_session(new_session);
     main_serve->add_acceptor(acc);
 
-    hare::io::console console;
+    auto& console = hare::io::console::instance();
     console.register_handle("quit", [=] {
         main_cycle->exit();
     });
-    console.attach(main_cycle);
+    console.attach(main_cycle.get());
 
-    LOG_INFO() << "========= ECHO serve start =========";
+    LOG_INFO(server_logger, "========= ECHO serve start =========");
 
     auto ret = main_serve->exec(1);
 
-    LOG_INFO() << "========= ECHO serve stop! =========";
+    LOG_INFO(server_logger, "========= ECHO serve stop =========");
 
     acc.reset();
 
