d793ee46a3c1f8d38ec02eff4e84d94d94fe476d
l1ang70 <gog_017@outlook.com>
2023-08-16 21:54:20 +0800

    [A] add TcpClient

---
+++ hare/net/tcp/client.cc
@@ -0,0 +1,222 @@
+#include "hare/base/fwd-inl.h"
+#include "hare/base/io/socket_op-inl.h"
+#include <hare/base/io/cycle.h>
+#include <hare/base/util/count_down_latch.h>
+#include <hare/net/tcp/client.h>
+
+namespace hare {
+namespace net {
+
+    HARE_IMPL_DEFAULT(
+        TcpClient,
+        std::string name {};
+
+        io::Cycle * cycle {};
+        bool started { false };
+
+        TcpSession::ConnectCallback connect_cb {};
+
+        Ptr<TcpSession> session {};
+        io::Event::Id retry_id { -1 };
+    )
+
+    TcpClient::TcpClient(io::Cycle* _cycle, std::string _name)
+        : impl_(new TcpClientImpl)
+    {
+        IMPL->name = std::move(_name);
+        IMPL->cycle = CHECK_NULL(_cycle);
+    }
+
+    TcpClient::~TcpClient()
+    {
+        Close();
+        delete impl_;
+    }
+
+    auto TcpClient::MainCycle() const -> io::Cycle*
+    {
+        return IMPL->cycle;
+    }
+
+    auto TcpClient::IsRunning() const -> bool
+    {
+        return IMPL->started;
+    }
+
+    void TcpClient::SetConnectCallback(TcpSession::ConnectCallback _connect)
+    {
+        IMPL->connect_cb = std::move(_connect);
+    }
+
+    auto TcpClient::Connected() -> bool
+    {
+        auto ret { false };
+        if (!MainCycle()->is_running()) {
+            return ret;
+        }
+
+        util::CountDownLatch cdl { 1 };
+        auto in_thread = MainCycle()->InCycleThread();
+
+        MainCycle()->RunInCycle([&] {
+            if (IMPL->session) {
+                ret = IMPL->session->Connected();
+            }
+            cdl.CountDown();
+        });
+
+        if (!in_thread) {
+            cdl.Await();
+        }
+
+        return ret;
+    }
+
+    void TcpClient::ConnectTo(const HostAddress& _address, bool _retry, std::int32_t _retry_times, std::int64_t _delay)
+    {
+        if (Connected()) {
+            HARE_INTERNAL_ERROR("the client is connected, please reconnect after disconnecting.");
+            return;
+        }
+        auto peer_addr = _address.Clone();
+        MainCycle()->QueueInCycle([=] {
+            auto family = peer_addr->Family();
+            auto fd = socket_op::CreateNonblockingOrDie(family);
+            auto ret = socket_op::Connect(fd, peer_addr->get_sockaddr(), socket_op::AddrLen(family));
+            auto saved_errno = !ret ? errno : 0;
+            switch (saved_errno) {
+            case 0:
+            case EINPROGRESS:
+            case EINTR:
+            case EISCONN:
+                IMPL->session.reset(new TcpSession(IMPL->cycle,
+                    HostAddress::LocalAddress(fd),
+                    fmt::format("{}-{}#tcp", IMPL->name, peer_addr->ToIpPort()),
+                    peer_addr->Family(), fd,
+                    std::move(*peer_addr)));
+                IMPL->session->SetDestroy(std::bind(&TcpClient::DestroySession, this));
+                IMPL->connect_cb(IMPL->session, SESSION_CONNECTED);
+                IMPL->session->ConnectEstablished();
+                IMPL->retry_id = -1;
+                break;
+
+            case EAGAIN:
+            case EADDRINUSE:
+            case EADDRNOTAVAIL:
+            case ECONNREFUSED:
+            case ENETUNREACH:
+                socket_op::Close(fd);
+                if (_retry && _retry_times > 0) {
+                    IMPL->retry_id = IMPL->cycle->RunAfter([=]() mutable {
+                        this->ConnectTo(*peer_addr, _retry, --_retry_times, _delay);
+                    },
+                        _delay);
+                } else if (_retry) {
+                    HARE_INTERNAL_ERROR("cannot connect to {}", peer_addr->ToIpPort());
+                    IMPL->retry_id = -1;
+                }
+                break;
+
+            case EACCES:
+            case EPERM:
+            case EAFNOSUPPORT:
+            case EALREADY:
+            case EBADF:
+            case EFAULT:
+            case ENOTSOCK:
+                socket_op::Close(fd);
+                HARE_INTERNAL_ERROR("a connection error {} occurred while connecting to {}", saved_errno, peer_addr->ToIpPort());
+                IMPL->retry_id = -1;
+                break;
+
+            default:
+                socket_op::Close(fd);
+                HARE_INTERNAL_ERROR("a unexpected error {} occurred while connecting to {}", saved_errno, peer_addr->ToIpPort());
+                IMPL->retry_id = -1;
+                break;
+            }
+        });
+    }
+
+    void TcpClient::Close()
+    {
+        util::CountDownLatch cdl { 1 };
+        MainCycle()->RunInCycle([&] {
+            if (IMPL->session) {
+                auto ret = IMPL->session->ForceClose();
+                if (!ret) {
+                    HARE_INTERNAL_ERROR("{}", ret.Description());
+                }
+            }
+            cdl.CountDown();
+        });
+
+        if (MainCycle()->InCycleThread()) {
+            cdl.Await();
+        }
+    }
+
+    auto TcpClient::Send(const void* _buffer, std::size_t _len) -> bool
+    {
+        if (!Connected()) {
+            return false;
+        }
+        Buffer buffer {};
+        auto ret = buffer.Add(_buffer, _len);
+        if (!ret) {
+            return ret;
+        }
+
+        util::CountDownLatch cdl { 1 };
+
+        MainCycle()->RunInCycle([&] {
+            if (IMPL->session) {
+                ret = IMPL->session->Append(buffer);
+            } else {
+                ret = false;
+            }
+            cdl.CountDown();
+        });
+
+        if (!MainCycle()->InCycleThread()) {
+            cdl.Await();
+        }
+
+        return ret;
+    }
+
+    auto TcpClient::Append(Buffer& _buffer) -> bool
+    {
+        if (!Connected()) {
+            return false;
+        }
+
+        auto ret { false };
+        util::CountDownLatch cdl { 1 };
+
+        MainCycle()->RunInCycle([&] {
+            if (IMPL->session) {
+                ret = IMPL->session->Append(_buffer);
+            } else {
+                ret = false;
+            }
+            cdl.CountDown();
+        });
+
+        if (!MainCycle()->InCycleThread()) {
+            cdl.Await();
+        }
+
+        return ret;
+    }
+
+    void TcpClient::DestroySession()
+    {
+        IMPL->cycle->AssertInCycleThread();
+        if (IMPL->session) {
+            IMPL->session.reset();
+        }
+    }
+
+} // namespace net
+} // namespace hare
---
+++ include/hare/net/tcp/client.h
@@ -0,0 +1,49 @@
+/**
+ * @file hare/net/tcp/client.h
+ * @author l1ang70 (gog_017@outlook.com)
+ * @brief Describe the class associated with client.h
+ * @version 0.1-beta
+ * @date 2023-04-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ **/
+
+#ifndef _HARE_NET_TCP_CLIENT_H_
+#define _HARE_NET_TCP_CLIENT_H_
+
+#include <hare/net/host_address.h>
+#include <hare/net/tcp/session.h>
+
+namespace hare {
+namespace net {
+
+    static const std::int64_t kRetryDelay = 1L * HARE_MICROSECONDS_PER_SECOND; // 1s
+
+    HARE_CLASS_API
+    class HARE_API TcpClient : public util::NonCopyable {
+        hare::detail::Impl* impl_ {};
+
+    public:
+        explicit TcpClient(io::Cycle* _cycle, std::string _name = "HARE_CLIENT");
+        virtual ~TcpClient();
+
+        auto MainCycle() const -> io::Cycle*;
+        auto IsRunning() const -> bool;
+        void SetConnectCallback(TcpSession::ConnectCallback _connect);
+
+        auto Connected() -> bool;
+        void ConnectTo(const HostAddress& _address, bool _retry = true, std::int32_t _retry_times = 30, std::int64_t _delay = kRetryDelay);
+        void Close();
+
+        auto Send(const void* _buffer, std::size_t _len) -> bool;
+        auto Append(Buffer& _buffer) -> bool;
+
+    private:
+        void DestroySession();
+    };
+
+} // namespace net
+} // namespace hare
+
+#endif // _HARE_NET_TCP_CLIENT_H_
